ee9fe40b253871da77ed4372ae5fcfa22f85e191
Mandana Dashti <dmandana59@gmail.com>
2024-10-09 14:37:05 +0330

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Read the manifest file from the public folder
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'public', 'manifest.json'), 'utf-8')
+);
+
 export default defineConfig({
-  plugins: [react()],
-  base: "/hangman"
+    plugins: [react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest
+        })
+    ],
+    base: "/hangman"
 })
